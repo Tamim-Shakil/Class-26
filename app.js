663ca8dbfd0801bb05d1d606b028f33863b0a258
Tamim-Shakil <tamim720sk@gmail.com>
2024-10-03 18:56:06 +0600

    Done

---
+++ app.js
@@ -0,0 +1,29 @@
+// function changeText() {
+//   document.querySelector("h1").textContent = "JavaScript is Async..";
+// }
+
+// const timer = setTimeout(changeText, 3000);
+
+// document.querySelector("#stop").addEventListener("click", () => {
+//   console.log(timer);
+//   clearTimeout(timer);
+//   console.log("timer stopped!");
+// });
+
+// function changeColor() {
+//   if (document.body.style.backgroundColor !== "black") {
+//     document.body.style.backgroundColor = "black";
+//     document.body.style.color = "white";
+//   } else {
+//     document.body.style.backgroundColor = "white";
+//     document.body.style.color = "black";
+//   }
+// }
+
+// // changeColor()
+
+// const interval = setInterval(changeColor, 1000)
+
+// document.querySelector("#stop").addEventListener("click", () => {
+//   clearInterval(interval)
+// });
